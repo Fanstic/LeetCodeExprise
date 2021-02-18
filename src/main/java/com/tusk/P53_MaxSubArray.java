b0aeae1cde93876204674fc7d7812eca136e72c8
tusk <843943878@qq.com>
2021-02-18 10:40:52 +0800

    改命名

--- src/main/java/com/tusk/P53_MaxSubArray.java
+++ src/main/java/com/tusk/P53_MaxSubArray.java
@@ -12,7 +12,7 @@ package com.tusk;
  * @desc 最大子序和
  * @date 2020/12/3 16:18
  */
-public class MaxSubArray {
+public class P53_MaxSubArray {
 
     public static void main(String[] args) {
         int[] arr = {-2,1,-3,4,-1,2,1,-5,4};
