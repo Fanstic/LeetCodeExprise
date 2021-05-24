77668af40e4806370e07ab6520653e7acc6fb727
tusk <843943878@qq.com>
2021-05-24 18:39:24 +0800

    1.二叉树先中后递归遍历 2.二叉树层级遍历 3.查找二叉树节点查找和插入

--- src/main/java/com/tusk/tree/BinaryTree.java
+++ src/main/java/com/tusk/tree/BinaryTree.java
@@ -1,6 +1,9 @@
 package com.tusk.tree;
 
+
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * @author tusk
@@ -9,12 +12,17 @@ import java.util.Arrays;
  */
 public class BinaryTree {
     public static void main(String[] args) {
-        int[] pre = new int[]{1, 2, 4, 7, 3, 5, 6, 8};
-        int[] mid = new int[]{4, 7, 2, 1, 5, 3, 8, 6};
+//        int[] pre = new int[]{1, 2, 4, 7, 3, 5, 6, 8};
+//        int[] mid = new int[]{4, 7, 2, 1, 5, 3, 8, 6};
+
+        int[] pre = new int[]{5, 4, 3, 2, 1, 6, 7, 8, 11};
+        int[] mid = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 11};
 
         BinaryTreeNode root = PM(pre, mid, pre.length);
+//        insert(root, 9);
+        levelTraversal(root);
 
-        System.out.println(root);
+
     }
 
     /**
@@ -87,7 +95,200 @@ public class BinaryTree {
         return root;
     }
 
+    /**
+     * 递归先序遍历 二叉树
+     *
+     * @param root
+     */
     public static void preOrderTraversal(BinaryTreeNode root) {
+        if (root == null) {
+            return;
+        }
 
+        System.out.println(root.key);
+        preOrderTraversal(root.left);
+        preOrderTraversal(root.right);
+    }
+
+    /**
+     * 递归中序遍历二叉树
+     *
+     * @param root
+     */
+    public static void midOrderTraversal(BinaryTreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        midOrderTraversal(root.left);
+        System.out.println(root.key);
+        midOrderTraversal(root.right);
+    }
+
+    /**
+     * 递归后续遍历二叉树
+     *
+     * @param root
+     */
+    public static void lastOrderTraversal(BinaryTreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        lastOrderTraversal(root.right);
+        lastOrderTraversal(root.left);
+        System.out.println(root.key);
+    }
+
+    /**
+     * 按照层级遍历
+     *
+     * @param root
+     */
+    public static void levelTraversal(BinaryTreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        Queue<BinaryTreeNode> queue = new LinkedList<>();
+
+
+        queue.add(root);
+
+        /*
+         * 打印当前节点,如果节点左节点不为null,将左节点入队,
+         * 如果右节点不为 null,将右节点入队
+         * 执行上述流程直至队列为空
+         */
+        while (!queue.isEmpty()) {
+            BinaryTreeNode node = queue.poll();
+            System.out.println(node.key);
+
+            if (node.left != null) {
+                queue.add(node.left);
+            }
+
+            if (node.right != null) {
+                queue.add(node.right);
+            }
+        }
+    }
+
+
+    //TODO:获取 key 在 二叉树 root 中的前驱节点
+
+    /**
+     * 获取 key 在 二叉树 root 中的前驱节点
+     *
+     * @param root
+     * @param key
+     * @param triversaltype 遍历的类型
+     * @return
+     */
+    public static BinaryTreeNode getPre(BinaryTreeNode root, int key, TRIVERSALTYPE triversaltype) {
+        return null;
+    }
+
+    //TODO:获取 key 在二叉树 root 中的后继节点
+
+    /**
+     * 获取 key 在二叉树 root 中的后继节点
+     *
+     * @param root
+     * @param key
+     * @param triversaltype 遍历类型
+     * @return
+     */
+    public static BinaryTreeNode getNext(BinaryTreeNode root, int key, TRIVERSALTYPE triversaltype) {
+        return null;
+    }
+
+    /**
+     * 在二叉查找树种查找key
+     *
+     * @param root
+     * @param key
+     * @return 查找成功返回 true,否则返回 false
+     */
+    public static BinaryTreeNode searchTree(BinaryTreeNode root, int key) {
+        if (root == null) {
+            return null;
+        }
+
+        if (key == root.key) {
+            return root;
+        } else if (root.left != null && key < root.key) {
+            return searchTree(root.left, key);
+        } else if (root.right != null && key > root.key) {
+            return searchTree(root.right, key);
+        }
+
+        return null;
+    }
+
+    /**
+     * 在二叉树中插入 key
+     *
+     * @param root
+     * @param key
+     * @return
+     */
+    public static BinaryTreeNode insert(BinaryTreeNode root, int key) {
+        if (root == null) {
+            root = new BinaryTreeNode();
+            root.key = key;
+
+            return root;
+        }
+
+        BinaryTreeNode node = searchTree(root, key);
+
+        if (node != null) {
+            return node;
+        } else {
+            return insertInternal(root, key);
+        }
+    }
+
+    private static BinaryTreeNode insertInternal(BinaryTreeNode root, int key) {
+
+        if (key < root.key) {
+            if (root.left == null) {
+                root.left = new BinaryTreeNode(key);
+                return root.left;
+            } else {
+                return insertInternal(root.left, key);
+            }
+        } else {
+            if (root.right == null) {
+                root.right = new BinaryTreeNode(key);
+                return root.right;
+            } else {
+                return insertInternal(root.right, key);
+            }
+
+        }
+    }
+
+    /**
+     * 遍历的类型
+     */
+    static enum TRIVERSALTYPE {
+        /**
+         * 先序
+         */
+        PRE,
+        /**
+         * 中序
+         */
+        MID,
+        /**
+         * 后续
+         */
+        LAST,
+        /**
+         * 层级遍历
+         */
+        LEVEL
     }
 }
--- src/main/java/com/tusk/tree/BinaryTreeNode.java
+++ src/main/java/com/tusk/tree/BinaryTreeNode.java
@@ -9,4 +9,18 @@ public class BinaryTreeNode {
     public int key;
     public BinaryTreeNode left;
     public BinaryTreeNode right;
+
+    public BinaryTreeNode() {
+
+    }
+
+    public BinaryTreeNode(int key) {
+        this.key = key;
+    }
+
+    public BinaryTreeNode(int key, BinaryTreeNode left, BinaryTreeNode right) {
+        this.key = key;
+        this.left = left;
+        this.right = right;
+    }
 }
